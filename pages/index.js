c8c54e93e6b1d85c0238eac7c94b0a8d54265854
Reach05 <147742053+Reach05@users.noreply.github.com>
2026-01-08 12:45:28 -0800

    Add Home component for SiteAdd basic home page at pages/index.js Selection Scaffold

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Site Selection Scaffold</h1>
+      <p>Welcome to the Site Selection Scaffold! This Next.js app includes GIS overlays using ArcGIS.</p>
+      <p>Go to <a href="/gis">GIS Overlay Page</a> to view sample map.</p>
+    </div>
+  );
+}
