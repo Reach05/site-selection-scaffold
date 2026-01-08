f3618c8b526ed586cf2a71d17e4f49d78b2394cf
Reach05 <147742053+Reach05@users.noreply.github.com>
2026-01-08 13:43:42 -0800

    Register ArcGIS custom elements in GIS page to fix arcgis-map error

--- pages/gis.js
+++ pages/gis.js
@@ -3,7 +3,10 @@ import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import { defineCustomElements } from '@arcgis/map-components/dist/loader';
 
-const ArcgisMap = dynamic(() => import('@arcgis/map-components-react').then(mod => mod.ArcgisMap), { ssr: false });
+const ArcgisMap = dynamic(
+  () => import('@arcgis/map-components-react').then((mod) => mod.ArcgisMap),
+  { ssr: false }
+);
 
 export default function Gis() {
   useEffect(() => {
@@ -14,7 +17,10 @@ export default function Gis() {
     <div>
       <h1>GIS Overlay Example</h1>
       <p>This page demonstrates a basic ArcGIS map overlay using a public web map item.</p>
-      <ArcgisMap itemId="f2e9b762544945f390ca4ac3671cfa72" style={{ height: '500px', width: '100%' }} />
+      <ArcgisMap
+        itemId="f2e9b762544945f390ca4ac3671cfa72"
+        style={{ height: '500px', width: '100%' }}
+      />
     </div>
   );
 }
